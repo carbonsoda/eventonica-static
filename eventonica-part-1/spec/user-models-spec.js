2900fbd6e7df1873b542f7240b4a54029f6e3966
Avery <22334165+carbonsoda@users.noreply.github.com>
2021-02-17 12:01:56 -0800

    Add basic User constructor tests

--- eventonica-part-1/spec/user-models-spec.js
+++ eventonica-part-1/spec/user-models-spec.js
@@ -5,6 +5,19 @@ const User  = models.User;
 // User Class only
 
 // Construction
+describe('User ', () => {
+    let a = new User('Avery');
+    let b = new User('Bill');
+    let c = new User('Carol');
+
+    it('Carol id == 202', () => {
+        expect(c.id).toBe(202);
+    });
+    it('all list should have 3 users', () => {
+        expect(User.all.length).toBe(3);
+    });
+    
+});
 
 // updateName(newName)
 
