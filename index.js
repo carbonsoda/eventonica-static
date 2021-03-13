1973ec808bc29523c7d390841a4a0f1b391dfbfb
Avery <22334165+carbonsoda@users.noreply.github.com>
2021-03-13 13:45:48 -0800

    Move back index.html + update index.js static reference

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('static'));
+app.use(express.static(__dirname + '/'));
 app.listen(port, () => {
     console.log(`Server running on port ${port}...`);
 });
